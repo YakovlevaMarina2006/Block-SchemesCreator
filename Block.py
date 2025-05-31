e21cc6c2116f65af5cf1adef9fd09c6ca6ab655c
YakovlevaMarina2006 <yakovlevamarina2006@gmail.com>
2025-05-31 20:35:42 +0700

    Blocks made resizable

--- Block.py
+++ Block.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
+# import MovementFuncs as MF
 
 class Block:
     def __init__(self, master, width, height, color, panel_color):
@@ -8,27 +9,52 @@ class Block:
         self.height = height
         self.color = color
         self.panel_color = panel_color
-        self.tk_obj = 0
-        self.panel = 0
+        self.block = Frame(master, bg=self.color, width=self.width, height=self.height)
+        self.panel = Frame(self.block, bg=self.panel_color, width=self.width, height=20)
+        self.text_frame = Frame(self.block, bg="black", width=self.width, height=self.height - 20)
+        self.text = ScrolledText(self.text_frame, bg=self.color, wrap="word")
+        self.panel2 = Frame(self.block, bg=self.panel_color, width=self.width, height=20)
+        self.del_button = Button(self.panel, text="X", command=lambda: self.delete())
+        self.resize_button = Button(self.panel2, text="X")
 
-    def create(self, master):
-        block = Frame(master, bg=self.color, width=self.width, height=self.height)
-        self.tk_obj = block
-        block.place(x=0, y=0)
-        panel = Frame(block, bg=self.panel_color, width=self.width, height=self.height//5)
-        panel.place(x=0, y=0)
+    def put(self, master):
 
-        btn = Button(panel, text="X", command=lambda: self.delete())
-        btn.place(x=self.width-20, y=0)
-        self.panel = panel
-        text = ScrolledText(block, bg=self.color, wrap="word")
-        text.place(x=0, y=20, width=self.width, height=self.height-20)
-        #text.pack(fill=BOTH, anchor=CENTER, expand=1)
-        #text_frame.place(x=0, y=30)
+        self.block.place(x=0, y=0)
+        for i in range(3): self.block.rowconfigure(index=i, weight=1)
 
-    def bind(self, *args):
-        self.panel.bind(*args)
+        self.panel.grid(row=0, column=0, sticky="nsew")
+
+        self.panel2.grid(row=2, column=0, sticky="nsew")
+
+        self.del_button.pack(anchor="e")
+
+        self.resize_button.pack(anchor="e")
+
+        self.text_frame.grid(row=1, column=0, sticky="nsew")
+        self.text_frame.columnconfigure(0, weight=10)
+        self.text_frame.rowconfigure(0, weight=10)
+        self.text_frame.grid_propagate(False)
+
+        self.text.grid(sticky="nsew")
+
+
+    # def make_draggable(self):
+    #     self.panel.bind('<Button-1>', MF.dnd_start)
+    #     self.panel.bind('<B1-Motion>', MF.dnd_motion)
+    #     self.panel2.bind('<Button-1>', MF.dnd_start)
+    #     self.panel2.bind('<B1-Motion>', MF.dnd_motion)
+    #
+    # def make_resizable(self):
+    #     self.resize_button.bind('<Button-1>', MF.dnd_start)
+    #     self.resize_button.bind('<B1-Motion>', MF.resize_motion)
 
     def delete(self):
         #self.tk_obj.pack_forget()
-        self.tk_obj.destroy()
+        self.block.destroy()
+
+    def resize(self, event):
+        widget = event.widget.master
+        self.width += -widget.startX + event.x
+        self.height += -widget.startY + event.y
+        self.text_frame.config(width=self.width)
+        self.text_frame.config(height=self.height)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from tkinter import *
 from Block import *
 
+#root = Tk()
+
 def get_window_size(window):
     data = window.geometry()
     return int(data.split("x")[0]), int(data.split("x")[1].split("+")[0])
@@ -24,32 +26,52 @@ def dnd_motion(event):
         y = 0
     elif y > get_window_size(root)[1] - widget.winfo_height():
         y = get_window_size(root)[1] - widget.winfo_height()
-    widget.place(x = x, y = y)
+    widget.place(x=x, y=y)
 
+def resize_motion(event):
+    widget = event.widget.master
+    block2.resize(x=-widget.startX + event.x, y=-widget.startY + event.y)
 
+def make_dragable(block):
+    block.panel.bind('<Button-1>', dnd_start)
+    block.panel.bind('<B1-Motion>', dnd_motion)
+    block.panel2.bind('<Button-1>', dnd_start)
+    block.panel2.bind('<B1-Motion>', dnd_motion)
+
+def make_resizable(block):
+    block.resize_button.bind('<Button-1>', dnd_start)
+    block.resize_button.bind('<B1-Motion>', block.resize)
 
 if __name__ == '__main__':
     root = Tk()  # создаем корневой объект - окно
     root.title("Block-Schemes Creator")  # устанавливаем заголовок окна
-
     max_screen_size_width = root.wm_maxsize()[0]
     max_screen_size_height = root.wm_maxsize()[1]
-    root.geometry(f"{max_screen_size_width//2}x{max_screen_size_height//2}+500+300")  # устанавливаем размеры окна
+    root.geometry(f"{max_screen_size_width // 2}x{max_screen_size_height // 2}+500+300")  # устанавливаем размеры окна
+
     # root.iconphoto(False, icon)
     root.update_idletasks()
-    canvas = Canvas(bg="white", width=get_window_size(root)[0] - 100, height=get_window_size(root)[1] - 100)
+    canvas = Canvas(bg="white")
     canvas.pack(fill=BOTH, anchor=CENTER, expand=1)
 
     block1 = Block(canvas, 300, 300, "pink", "red")
-    block1.create(canvas)
-    block1.bind('<Button-1>', dnd_start)
-    block1.bind('<B1-Motion>', dnd_motion)
+    block1.put(canvas)
+    make_dragable(block1)
+    make_resizable(block1)
+    #block1.make_draggable()
+    #block1.make_draggable()
 
-    block2 = Block(canvas, 100, 100, "light blue", "purple")
-    block2.create(canvas)
-    block2.bind('<Button-1>', dnd_start)
-    block2.bind('<B1-Motion>', dnd_motion)
+    block2 = Block(canvas, 200, 100, "light blue", "purple")
+    block2.put(canvas)
+    make_dragable(block2)
+    make_resizable(block2)
+    #block2.make_draggable()
+    #block2.resize(x=100,y=100)
+    # block2.panel.bind('<Button-1>', dnd_start)
+    # block2.panel.bind('<B1-Motion>', dnd_motion)
+    # block2.resize_button.bind('<Button-1>', dnd_start)
+    # block2.resize_button.bind('<B1-Motion>', block2.resize)
+    #block2.make_resizable()
+    #block2.make_resizable()
     #block2.delete()
-
-
     root.mainloop()
